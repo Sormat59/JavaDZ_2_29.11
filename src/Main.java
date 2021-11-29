541997e59edc37bdd17156e05d115d99aa1ce9e2
Sarmat <def2413@bk.ru>
2021-11-29 22:11:38 +0500

    BonusSustem

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int currentBalance = 100;
-        int replenishment = 200;
+        int currentBalance = 300;
+        int replenishment = 2013;
         int newBalance = replenishment + currentBalance;
         if (replenishment > 1000) {
             newBalance = replenishment / 100 * 1 + replenishment + currentBalance;
         }
-        System.out.println("Баланс = " + newBalance);
+        System.out.println("Ваш Баланс = " + newBalance);
     }
 }
